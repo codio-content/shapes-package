6dd9c0fce54f8bccc9d5771c76732324e7fba8d2
Patrick <patrick.ester@gmail.com>
2022-04-22 11:27:29 -0400

    add setup.py
    Add the setup.py file and restructure the package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='shapes',
+   version='1.0',
+   description='An example package',
+   author='Calvin & Hobbes',
+   author_email='calvin@hobbes.com',
+   packages=['shapes'],  
+   install_requires=[],
+)
